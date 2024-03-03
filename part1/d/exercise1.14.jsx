144e82dc965283d8aa4ca8efda7a561b20a8e823
danigalron <161462392+danigalron@users.noreply.github.com>
2024-03-04 00:02:45 +0100

    exercise1.14.jsx

---
+++ part1/d/exercise1.14.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const Button = (props) => (
+  <button onClick={props.onTouch}>
+    {props.text}
+  </button>
+);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast is to go well.'
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length));
+  const maxIndex = points.indexOf(Math.max(...points));
+
+  const setRandom = () => {
+    setSelected(getRandomInt(anecdotes.length));
+  };
+
+  const setNewPoint = () => {
+    const newPoints = [...points];
+    newPoints[selected] += 1;
+    setPoints(newPoints);
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br />
+      <Button text="Next anecdote" onTouch={setRandom} />
+      <Button text="Vote" onTouch={setNewPoint} />
+      <p>This anecdote has {points[selected]} votes</p>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[maxIndex]}</p>
+    </div>
+  );
+};
+
+export default App;
