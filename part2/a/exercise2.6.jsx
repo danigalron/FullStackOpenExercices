e2f9d61feea56b22a0b7fb3d747f050c6b6eadc7
danigalron <161462392+danigalron@users.noreply.github.com>
2024-03-05 19:23:11 +0100

    exercise2.6.jsx

---
+++ part2/a/exercise2.6.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const newObject = {
+      name : newName
+    }
+    setPersons(persons.concat(newObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, key) =>
+        <li key = {key}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
